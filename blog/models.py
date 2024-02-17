8091f2ebd70170cf1cb31de9887e18eb882bf446
IlyA1536 <i0977187143@gmailcom>
2024-02-17 12:12:10 +0200

    Add files and code from MIchael

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
-
+# Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length = 63)
     content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now = True)
+    photo = models.ImageField(default=None)
+
+    def __str__(self):
+        return self.title
+
